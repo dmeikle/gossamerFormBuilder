5efcde44d30efbcc5fabbbee99b9dd288e838730
Dave Meikle <davemeikle@ymail.com>
2014-11-17 12:10:24 -0800

    fixed missing values on passed params when failed validation

--- src/Gossamer/CMS/Forms/Controls/AbstractControl.php
+++ src/Gossamer/CMS/Forms/Controls/AbstractControl.php
@@ -12,25 +12,30 @@ abstract class AbstractControl {
     public abstract function build($name, array $params = null, &$validationResults = null);
     
     protected function buildParams($fieldName, &$control, array $params = null, &$validationResults) {
-        if(is_null($params)) {
-            if(is_array($validationResults) && array_key_exists($fieldName . '_value', $validationResults)) {
+        if(is_null($params)) {   
+            if(is_array($validationResults) && array_key_exists($fieldName . '_value', $validationResults)) {               
                 $value = ' value="' . $validationResults[$fieldName . '_value'] . '"';
                 $control = str_replace('|PARAMS|', $value, $control);
-            } else {
+            } else {               
                 $control = str_replace('|PARAMS|', '', $control);
             }            
             
             return;
         }
+       
         $valueSet = false;
-        
         $paramList = '';
         foreach($params as $key => $param) {
             if($key == 'value') {
-                $paramList .= " $key=\"" . $this->formatValue($fieldName, $param, $validationResults) . "\"";
+                $paramList .= " value=\"" . $this->formatValue($fieldName, $param, $validationResults) . "\"";
+                $valueSet = true;
             } else {
                 $paramList .= " $key=\"$param\"";
             }
+        }
+        
+        if(!$valueSet && array_key_exists($fieldName . '_value', $validationResults)) {
+            $paramList .= " value=\"" . $this->formatValue($fieldName, $param, $validationResults) . "\"";
         }
         
         $control = str_replace('|PARAMS|', $paramList, $control);
--- tests/Gossamer/CMS/Forms/FormBuilderTest.php
+++ tests/Gossamer/CMS/Forms/FormBuilderTest.php
@@ -14,47 +14,47 @@ class FormBuilderTest extends \tests\BaseTest{
     public function testAddTextBox() {
         $builder = new FormBuilder($this->getLogger());
         $results = array('firstname' => 'VALIDATION_INVALID_EMAIL',
-             'firstname_value' => 'davemmyemail.com',
+             'firstname_value' => 'invaliddavemmyemail.com',
             'test' =>'SOME_FAIL_ON_TEST',
             'test_value' => 'some fail value');
         $builder->addValidationResults($results);
         
         $control = $builder->add('test2', 'radio', array('value' => 'dave meikle'))
                 ->add('test1','text', array('value' => 'dave meikle', 'id' => 'test1'))
-                ->add('firstname', 'text', array('value' => 'override me', 'id' => 'test1'))
-                ->add('lastname', 'text');
+                ->add('firstname', 'text', array('id' => 'firstname_id'));
+//                /->add('lastname', 'text');
         
         $form = $control->getForm();
         print_r($form);
     }
-    
-    public function testAddRadioButton() {
-        $builder = new FormBuilder($this->getLogger());
-         $results = array('firstname' => 'VALIDATION_INVALID_EMAIL',
-             'firstname_value' => 'davemmyemail.com',
-            'test' =>'SOME_FAIL_ON_TEST',
-            'test_value' => 'some fail value');
-        $builder->addValidationResults($results);
-        
-        $control = $builder->add('test2', 'radio', array('value' => 'dave meikle'))
-                ->add('test1','text', array('value' => 'dave meikle', 'id' => 'test1'))
-                ->add('firstname', 'text', array('value' => 'override me', 'id' => 'test1'))
-                ->add('lastname', 'text');
-        
-        $form = $control->getForm();
-        print_r($form);
-    }
-    
-    public function testAddTextBoxFailedValidation() {
-        $builder = new FormBuilder($this->getLogger());
-        $results = array('firstname' => 'SOME_STRING',
-            'test' =>'SOME_FAIL_ON_TEST');
-        $builder->addValidationResults($results);
-        $control = $builder->add('test', 'text', array('value' => 'dave meikle', 'id' => 'test'));
-        
-        $form = $control->getForm();
-        print_r($form);
-    }
+//    
+//    public function testAddRadioButton() {
+//        $builder = new FormBuilder($this->getLogger());
+//         $results = array('firstname' => 'VALIDATION_INVALID_EMAIL',
+//             'firstname_value' => 'davemmyemail.com',
+//            'test' =>'SOME_FAIL_ON_TEST',
+//            'test_value' => 'some fail value');
+//        $builder->addValidationResults($results);
+//        
+//        $control = $builder->add('test2', 'radio', array('value' => 'dave meikle'))
+//                ->add('test1','text', array('value' => 'dave meikle', 'id' => 'test1'))
+//                ->add('firstname', 'text', array( 'id' => 'test1'))
+//                ->add('lastname', 'text');
+//        
+//        $form = $control->getForm();
+//        print_r($form);
+//    }
+//    
+//    public function testAddTextBoxFailedValidation() {
+//        $builder = new FormBuilder($this->getLogger());
+//        $results = array('firstname' => 'SOME_STRING',
+//            'test' =>'SOME_FAIL_ON_TEST');
+//        $builder->addValidationResults($results);
+//        $control = $builder->add('test', 'text', array('value' => 'dave meikle', 'id' => 'test'));
+//        
+//        $form = $control->getForm();
+//        print_r($form);
+//    }
     
 /*
  * $this->createFormBuilder($server)
