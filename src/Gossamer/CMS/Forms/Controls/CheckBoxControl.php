d1c0660ef48e702a8a2268af9594891f77bf7dd2
Dave Meikle <david@quantumunit.com>
2014-11-21 16:06:17 -0800

    added checkbox control

---
+++ src/Gossamer/CMS/Forms/Controls/CheckBoxControl.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Gossamer\CMS\Forms\Controls;
+
+use Gossamer\CMS\Forms\Controls\AbstractControl;
+
+/**
+ * Description of TextBox
+ *
+ * @author Dave Meikle
+ */
+class CheckBoxControl extends AbstractControl {
+    
+    private $textBox = '<input type="checkbox" name="|NAME|"|PARAMS| />';
+    
+    public function build($name, array $params = null, &$validationResults = null, $wrapperName = null) {
+         if(!is_null($wrapperName)) {
+            $textBox = str_replace('|NAME|', $wrapperName . '[' . $name . ']', $this->textBox);
+        } else {
+            $textBox = str_replace('|NAME|', $name, $this->textBox);
+        }
+        
+        $this->buildParams($name, $textBox, $params, $validationResults, $wrapperName);
+        
+        return $textBox;
+    }
+
+    
+}
