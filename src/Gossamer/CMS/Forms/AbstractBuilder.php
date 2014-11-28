7cb4306447757e3d56532301cc50171704869182
Dave Meikle <david@quantumunit.com>
2014-11-28 10:12:05 -0800

    added locales list builder

--- src/Gossamer/CMS/Forms/AbstractBuilder.php
+++ src/Gossamer/CMS/Forms/AbstractBuilder.php
@@ -3,11 +3,20 @@
 
 namespace Gossamer\CMS\Forms;
 
+use Gossamer\Caching\CacheManager;
+use core\AbstractModel;
+
 /**
  *
  * @author davem
  */
 abstract class AbstractBuilder {
+    
+    private $locales = array();
+    
+    public function setLocales(array $locales) {
+        $this->locales = $locales;
+    }
     
     abstract public function buildForm(FormBuilder $builder, array $values = null, array $options = null, array $validationResults = null);
     
@@ -20,4 +29,29 @@ abstract class AbstractBuilder {
         return $result[$key];
     }
     
+    public function getLocalesFields(AbstractModel $model) {
+        $fields = $model->getEmptyModelStructure();
+        
+        foreach($fields as $row) {
+            if(array_key_exists('locales', $rows)) {
+                
+                return $this->drawLocalesColumns($locales, $row['locales']);                
+            }
+        }
+        
+        return null;
+    }
+    
+    private function drawLocalesColumns(array $locales, array $row) {
+        $retval = array();
+        
+        foreach($this->locales as $locale) {
+            $key = $locale['locale'];
+            foreach($row as $column) {
+                $retval[$key][$column['Field']] = '';
+            }            
+        }
+        
+        return $retval;
+    }
 }
