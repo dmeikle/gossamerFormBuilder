4e612939592ed2c7f7172bb374949b83359f3eec
Dave Meikle <david@quantumunit.com>
2015-01-28 09:19:59 -0800

    added const names to avoid control name mistakes by developers

--- src/Gossamer/CMS/Forms/Factory/ControlFactory.php
+++ src/Gossamer/CMS/Forms/Factory/ControlFactory.php
@@ -8,14 +8,28 @@ namespace Gossamer\CMS\Forms\Factory;
  * @author Dave Meikle
  */
 class ControlFactory {
-    
+
     private $controls = array();
-    
+
+    const TEXTBOX = 'text';
+    const RADIO = 'radio';
+    const CHECKBOX = 'check';
+    const HIDDEN = 'hidden';
+    const TEXTAREA = 'textarea';
+    const SUBMIT = 'submit';
+    const BUTTON = 'button';
+    const CANCEL = 'cancel';
+    const EMAIL = 'email';
+    const PASSWORD = 'passsword';
+    const SELECT = 'select';
+    const FILE = 'file';
+    const SPAN = 'span';
+    const PLACEHOLDER = 'placeholder';
+
     private $namingConvention = array(
         'text' => 'TextBox',
         'radio' => 'RadioButton',
         'check' => 'CheckBox',
-        'hidden' => 'HiddenInput',
         'textarea' => 'TextArea',
         'submit' => 'SubmitButton',
         'button' => 'Button',
@@ -28,13 +42,14 @@ class ControlFactory {
         'placeholder' => 'PlaceHolder',
         'hidden' => 'Hidden'
     );
-    
+
     public function getControl($controlName) {
-        if(!array_key_exists($controlName, $this->controls)) {
+        if (!array_key_exists($controlName, $this->controls)) {
             $control = 'Gossamer\\CMS\\Forms\\Controls\\' . $this->namingConvention[$controlName] . 'Control';
             $this->controls[$controlName] = new $control();
         }
-      
+
         return $this->controls[$controlName];
     }
+
 }
